9a0f940314899da252b917e2b585de23f5435341
apdiaz <alvaro.diaz.v@qcode.cl>
2023-04-26 12:46:31 -0400

    CompundedOvernightRateCashflow inicializa el interés correctamente utilizando el spread.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="qc_financial",
-    version="0.1.7",
+    version="0.2.0",
     author="Alvaro Díaz",
     author_email="alvaro@efaa.cl",
     description="Valuation of linear FX and Interest Rate Derivatives",
